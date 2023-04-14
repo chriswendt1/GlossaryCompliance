37cfabdab1f5c580ed8aa455deac63144dcd7888
Chris Wendt <chris.wendt@outlook.com>
2023-04-14 10:55:17 -0700

    Fixed the counting logic. Added a report writer.

---
+++ ReportWriter.cs
@@ -0,0 +1,27 @@
+﻿using System.Diagnostics;
+
+namespace GlossaryCompliance
+{
+    internal class ReportWriter
+    {
+        public string FileName { get; }
+        public ReportWriter(string filename)
+        {
+            FileName = filename;
+            File.WriteAllText(FileName, $"{DateTime.Now.ToShortDateString()} {DateTime.Now.ToShortTimeString()}");
+        }
+
+        public void WriteLine(string text)
+        {
+            File.AppendAllText(FileName, text + "\r\n");
+        }
+
+        public void Show()
+        {
+            using Process process = new();
+            process.StartInfo.FileName = "explorer";
+            process.StartInfo.Arguments = FileName;
+            process.Start();
+        }
+    }
+}
